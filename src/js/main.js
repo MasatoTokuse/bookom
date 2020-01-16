d22fb418b231307b01db6ff87372d0aef6db57ad
tokuse.masato <29adf6b4-6e73-4c96-a39f-226fd1fa8cb6@localhost>
2020-01-16 15:26:18 +0900

    Add userID and posted_at in post

--- src/js/main.js
+++ src/js/main.js
@@ -3,9 +3,11 @@
 let postComponent = Vue.extend({
   props: {
     index: Number,
+    userID: String,
     text: String,
     image: String,
-    comments: Array
+    comments: Array,
+    posted_at: String
   },
   data: function () {
     return {
@@ -13,8 +15,10 @@ let postComponent = Vue.extend({
     }
   },
   template: '\
-    <li>\
-      <img :src="image">\
+    <li class="post">\
+      <strong>投稿者 : {{ userID }} </strong>\
+      {{ posted_at }}<br>\
+      <img class="image" :src="image"><br>\
       <p class="accept-line">{{ text }}</p>\
       <div class="comments">\
         <ul>\
@@ -24,7 +28,7 @@ let postComponent = Vue.extend({
         </ul>\
         <form @submit.prevent="addComment">\
           <textarea v-model="newComment"></textarea>\
-          <input type="submit" value="投稿"><br>\
+          <input type="submit" value="返信"><br>\
         </form>\
       </div>\
     </li>\
@@ -53,6 +57,7 @@ let app = new Vue({
     newPost: '',
     newImage: '',
     posts: [{
+      userID: 'Tokuse',
       text: 'iizo!',
       image: '',
       comments: [{
@@ -60,7 +65,8 @@ let app = new Vue({
       },
       {
         text: 'コメント２'
-      }]
+      }],
+      posted_at: '2019/01/16 15:19:20'
     }]
   },
   components: {
@@ -74,11 +80,16 @@ let app = new Vue({
       if (this.newPost == '') {
         return
       }
+      let posted_at = getNowDateString();
+      // console.log(posted_at);
       let postItem = {
+        userID: this.userID,
         text: this.newPost,
         image: this.newImage,
-        comments: []
+        comments: [],
+        posted_at: posted_at
       };
+      // 新着順に表示するため、配列の先頭に追加する
       this.posts.unshift(postItem);
 
       // 投稿フォームのクリア
@@ -100,3 +111,32 @@ let app = new Vue({
     }
   }
 });
+
+//日付から文字列に変換する関数
+function getNowDateString() {
+
+  let date = new Date();
+  let year_str = date.getFullYear();
+  //月だけ+1すること
+  let month_str = 1 + date.getMonth();
+  let day_str = date.getDate();
+  let hour_str = date.getHours();
+  let minute_str = date.getMinutes();
+  let second_str = date.getSeconds();
+
+  month_str = ('0' + month_str).slice(-2);
+  day_str = ('0' + day_str).slice(-2);
+  hour_str = ('0' + hour_str).slice(-2);
+  minute_str = ('0' + minute_str).slice(-2);
+  second_str = ('0' + second_str).slice(-2);
+
+  let format_str = 'YYYY-MM-DD hh:mm:ss';
+  format_str = format_str.replace(/YYYY/g, year_str);
+  format_str = format_str.replace(/MM/g, month_str);
+  format_str = format_str.replace(/DD/g, day_str);
+  format_str = format_str.replace(/hh/g, hour_str);
+  format_str = format_str.replace(/mm/g, minute_str);
+  format_str = format_str.replace(/ss/g, second_str);
+
+  return format_str;
+};

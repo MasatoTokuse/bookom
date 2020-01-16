7fd4cae5bc12e5c3d566089975267d67fc2b417e
tokuse.masato <29adf6b4-6e73-4c96-a39f-226fd1fa8cb6@localhost>
2020-01-16 12:58:54 +0900

    Initial commit

---
+++ src/js/main.js
@@ -0,0 +1,91 @@
+'use strict'
+
+let postComponent = Vue.extend({
+  props: {
+    index: Number,
+    text: String,
+    image: String,
+    comments: Array
+  },
+  data: function () {
+    return {
+      newComment: ''
+    }
+  },
+  template: '\
+    <li>\
+      <img :src="image">\
+      <p>{{ text }}</p>\
+      <div class="comments">\
+        <ul>\
+          <li v-for="comment in comments">\
+              <p>{{ comment.text }}</p>\
+          </li>\
+        </ul>\
+        <form @submit.prevent="addComment">\
+          <input type="text" v-model="newComment">\
+          <input type="submit" value="投稿"><br>\
+        </form>\
+      </div>\
+    </li>\
+  ',
+  methods: {
+    addComment: function () {
+      let commentItem = {
+        text: this.newComment
+      }
+      this.$emit('add-comment', this.index, commentItem)
+
+      // 投稿フォームのクリア
+      this.newComment = ''
+    }
+  }
+});
+
+let app = new Vue({
+  el: '#app',
+  data: {
+    newPost: '',
+    newImage: '',
+    posts: [{
+      text: 'iizo!',
+      image: '',
+      comments: [{
+        text: 'コメント１'
+      },
+      {
+        text: 'コメント２'
+      }]
+    }]
+  },
+  components: {
+    'post-component': postComponent
+  },
+  methods: {
+    addPost: function () {
+      let postItem = {
+        text: this.newPost,
+        image: this.newImage,
+        comments: []
+      }
+      this.posts.unshift(postItem);
+
+      // 投稿フォームのクリア
+      this.newPost = ''
+      this.newImage = ''
+    },
+    // 画像をData URI Schemeに変換
+    onFileChange: function (e) {
+      let files = e.target.files || e.dataTransfer.files;
+      let reader = new FileReader();
+      reader.onload = (e) => {
+        this.newImage = e.target.result;
+        // console.log(this.newImage)
+      };
+      reader.readAsDataURL(files[0]);
+    },
+    addCommentToPost: function(index, comment) {
+      this.posts[index].comments.push(comment);
+    }
+  }
+});
